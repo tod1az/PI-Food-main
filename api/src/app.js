32bcc5464de8e805f4c3c2bd235b0fe397b77ed2
tod1az <t.diaz.soto@gmail.com>
2025-07-07 10:40:55 -0400

    feat: take cors allowed urls from .env

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const routes = require('./routes/index.js')
-
+require('dotenv').config()
 require('./db.js')
 
 const server = express()
@@ -14,8 +14,8 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 server.use(bodyParser.json({ limit: '50mb' }))
 server.use(cookieParser())
 server.use(morgan('dev'))
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173') // update to match the domain you will make the request from
+server.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL)
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
@@ -24,7 +24,6 @@ server.use((req, res, next) => {
 
 server.use('/', routes)
 
-// Error catching endware.
 server.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
   const status = err.status || 500
